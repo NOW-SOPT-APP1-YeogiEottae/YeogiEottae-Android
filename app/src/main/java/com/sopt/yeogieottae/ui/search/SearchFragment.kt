4365aa53a840f4bb7f3c2cbfc74437992877dc40
2hyunjinn <95455569+2hyunjinn@users.noreply.github.com>
2024-05-16 06:07:46 +0900

    [Feat] 매진 숙소 필터 구현

--- app/src/main/java/com/sopt/yeogieottae/ui/search/SearchFragment.kt
+++ app/src/main/java/com/sopt/yeogieottae/ui/search/SearchFragment.kt
@@ -20,8 +20,12 @@ class SearchFragment : BaseFragment<FragmentSearchBinding>(
 
         // 호텔 리사이클러뷰 연결
         initHotelListView()
+
+        // 매진숙소 필터 설정
+        initFilterExceptView()
     }
 
+    // 호텔 리사이클러 뷰
     private fun initHotelListView() {
         // 호텔 리사이클러뷰 설정
         val recyclerView: RecyclerView = requireView().findViewById(R.id.rv_searchlist_hotellist)
@@ -35,5 +39,32 @@ class SearchFragment : BaseFragment<FragmentSearchBinding>(
                 adapter.setHotels(it)
             }
         }
+    }
+
+    // 매진 숙소 필터
+    private fun initFilterExceptView() {
+        // RecyclerView 스크롤 이벤트 처리
+        val recyclerView: RecyclerView = requireView().findViewById(R.id.rv_searchlist_hotellist)
+        recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            private var isScrolledUp = false
+
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                // 스크롤 방향에 따라 아래 이미지뷰 숨기기 또는 표시하기
+                if (dy > 0 && !isScrolledUp) {
+                    // 아래로 스크롤할 때
+                    // 아래 이미지뷰 숨기기
+                    isScrolledUp = true
+                    view?.findViewById<View>(R.id.iv_searchlist_filter_except)?.animate()?.alpha(0f)
+                        ?.setDuration(300)?.start()
+                } else if (dy < 0 && isScrolledUp) {
+                    // 위로 스크롤할 때
+                    // 아래 이미지뷰 표시하기
+                    isScrolledUp = false
+                    view?.findViewById<View>(R.id.iv_searchlist_filter_except)?.animate()?.alpha(1f)
+                        ?.setDuration(300)?.start()
+                }
+            }
+        })
     }
 }
