7c8ee3fd687b14a9475c9f2f3b81241651273462
0se0 <030ggong@gmail.com>
2024-05-17 03:35:54 +0900

    [Feat]#6 - CompareViewModel에 따른 CompareFragment 이동 구현

--- app/src/main/java/com/sopt/yeogieottae/ui/compare/CompareFragment.kt
+++ app/src/main/java/com/sopt/yeogieottae/ui/compare/CompareFragment.kt
@@ -2,13 +2,31 @@ package com.sopt.yeogieottae.ui.compare
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.commit
+import androidx.fragment.app.viewModels
+import com.sopt.yeogieottae.R
 import com.sopt.yeogieottae.databinding.FragmentCompareBinding
+import com.sopt.yeogieottae.ui.compare.empty.CompareEmptyFragment
 import com.sopt.yeogieottae.util.BaseFragment
 
 class CompareFragment : BaseFragment<FragmentCompareBinding>(
     FragmentCompareBinding::inflate
 ) {
+    private val viewModel: CompareViewModel by viewModels()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        viewModel.apiResponse.observe(viewLifecycleOwner) { apiResponse ->
+            if (apiResponse.data.isEmpty()) {
+                parentFragmentManager.commit {
+                    replace(R.id.compare_fcv, CompareEmptyFragment())
+                }
+            } else {
+                parentFragmentManager.commit {
+                    replace(R.id.compare_fcv, CompareNotEmptyFragment())
+                }
+            }
+        }
     }
 }
