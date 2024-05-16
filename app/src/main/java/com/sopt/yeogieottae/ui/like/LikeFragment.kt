44e65af7f9d9673b7fdebd5408269fda10b62c1a
0se0 <030ggong@gmail.com>
2024-05-17 03:34:28 +0900

    [Feat]#6 - 객실비교로 화면 연결

--- app/src/main/java/com/sopt/yeogieottae/ui/like/LikeFragment.kt
+++ app/src/main/java/com/sopt/yeogieottae/ui/like/LikeFragment.kt
@@ -2,13 +2,22 @@ package com.sopt.yeogieottae.ui.like
 
 import android.os.Bundle
 import android.view.View
+import androidx.navigation.fragment.findNavController
+import com.sopt.yeogieottae.R
 import com.sopt.yeogieottae.databinding.FragmentLikeBinding
 import com.sopt.yeogieottae.util.BaseFragment
 
-class LikeFragment: BaseFragment<FragmentLikeBinding> (
+class LikeFragment : BaseFragment<FragmentLikeBinding>(
     FragmentLikeBinding::inflate
-){
+) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initCompareButton()
+    }
+
+    private fun initCompareButton() {
+        binding.compareBtn.setOnClickListener {
+            findNavController().navigate(R.id.action_like_to_compare)
+        }
     }
 }
