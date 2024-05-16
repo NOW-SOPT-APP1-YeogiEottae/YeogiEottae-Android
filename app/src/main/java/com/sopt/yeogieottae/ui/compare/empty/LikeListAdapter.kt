90ad1a94f3f2b52d0b4a84bc3e3f9316dca71d91
0se0 <030ggong@gmail.com>
2024-05-17 03:38:10 +0900

    [Feat]#6 - 찜 목록 조회를 위한 리사이클러뷰

---
+++ app/src/main/java/com/sopt/yeogieottae/ui/compare/empty/LikeListAdapter.kt
@@ -0,0 +1,37 @@
+package com.sopt.yeogieottae.ui.compare.empty
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import com.sopt.yeogieottae.data.RoomInformation
+import com.sopt.yeogieottae.databinding.ItemLikeListBinding
+
+class LikeListAdapter(private val onCheckedChanged: (Boolean) -> Unit) :
+    ListAdapter<RoomInformation, LikeListViewHolder>(DiffCallback()) {
+
+    private val selectedItems = mutableSetOf<Int>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LikeListViewHolder {
+        val binding =
+            ItemLikeListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return LikeListViewHolder(binding, selectedItems, onCheckedChanged)
+    }
+
+    override fun onBindViewHolder(holder: LikeListViewHolder, position: Int) {
+        val item = getItem(position)
+        holder.bind(item, position)
+    }
+
+    class DiffCallback : DiffUtil.ItemCallback<RoomInformation>() {
+        override fun areItemsTheSame(oldItem: RoomInformation, newItem: RoomInformation): Boolean {
+            return oldItem.roomId == newItem.roomId
+        }
+
+        override fun areContentsTheSame(
+            oldItem: RoomInformation, newItem: RoomInformation,
+        ): Boolean {
+            return oldItem == newItem
+        }
+    }
+}
