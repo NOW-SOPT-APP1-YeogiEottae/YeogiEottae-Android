842e8b01d89981f239c17fc82fd926d785afec3b
김명석 <75840431+cacaocoffee@users.noreply.github.com>
2024-05-16 19:07:08 +0900

    [Feat] #3 Room 뷰 연결 완료

--- app/src/main/java/com/sopt/yeogieottae/ui/room/RoomFragment.kt
+++ app/src/main/java/com/sopt/yeogieottae/ui/room/RoomFragment.kt
@@ -1,14 +1,123 @@
 package com.sopt.yeogieottae.ui.room
 
+
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.style.BulletSpan
+import android.text.style.ForegroundColorSpan
+import android.text.style.TextAppearanceSpan
 import android.view.View
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.viewModels
+import com.sopt.yeogieottae.R
 import com.sopt.yeogieottae.databinding.FragmentRoomBinding
 import com.sopt.yeogieottae.util.BaseFragment
+
 
 class RoomFragment : BaseFragment<FragmentRoomBinding>(
     FragmentRoomBinding::inflate
 ) {
+    private val roomViewModel: RoomViewModel by viewModels()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
+
+    private fun initView() {
+        with(binding) {
+            tvRoomDetailRoomName.text = roomViewModel.Room.value?.room_name
+            tvRoomSummaryDiscountPrice.text = roomViewModel.Room.value?.price.toString()
+            tvRoomDetailBottomPrice.text = roomViewModel.Room.value?.price.toString()
+            tvRoomSummaryTime.text =
+                "입실 ${roomViewModel.Room.value?.start_time} 퇴실 ${roomViewModel.Room.value?.end_time}"
+
+            tvRoomDetailNormalInfoDescription.text =
+                createSpannedText(roomViewModel.detail.value?.information ?: emptyList())
+            tvRoomDetailFacilitiesDescription.text =
+                createSpannedText(roomViewModel.detail.value?.facilities ?: emptyList())
+            tvRoomDetailCancelDescription.text =
+                createSpannedTextCancel(roomViewModel.detail.value?.cancel ?: emptyList())
+        }
+    }
+
+    private fun createSpannedText(
+        texts: List<String>,
+        bulletColor: Int = ContextCompat.getColor(requireContext(), R.color.gray_700)
+    ): SpannableStringBuilder {
+        val spannableStringBuilder = SpannableStringBuilder()
+
+        for ((index, text) in texts.withIndex()) {
+            val spannableString = SpannableString(text)
+            val bulletSpan = BulletSpan(3, bulletColor, 1)
+
+            spannableString.setSpan(
+                bulletSpan,
+                0,
+                spannableString.length,
+                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+            spannableStringBuilder.append(spannableString)
+            if (index < texts.size - 1) {
+                spannableStringBuilder.append("\n")
+            }
+        }
+        return spannableStringBuilder
+    }
+
+    private fun createSpannedTextCancel(
+        texts: List<String>,
+        bulletColor: Int = ContextCompat.getColor(requireContext(), R.color.gray_700)
+    ): SpannableStringBuilder {
+        val spannableStringBuilder = SpannableStringBuilder()
+        for ((index, text) in texts.withIndex()) {
+            val spannableString = SpannableString(text)
+            val bulletSpan = BulletSpan(3, bulletColor, 1)
+
+            spannableString.setSpan(
+                bulletSpan,
+                0,
+                spannableString.length,
+                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+            if (index == 3) {
+                spannableString.setSpan(
+                    ForegroundColorSpan(ContextCompat.getColor(requireContext(), R.color.cuation)),
+                    0,
+                    spannableString.length,
+                    Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+                )
+                spannableString.setSpan(
+                    TextAppearanceSpan(
+                        requireContext(),
+                        R.style.TextAppearance_Yeogieotte_Pretendard_h5
+                    ),
+                    0,
+                    spannableString.length,
+                    Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                )
+            }
+
+            if (index in 4..5) {
+                spannableString.setSpan(
+                    TextAppearanceSpan(
+                        requireContext(),
+                        R.style.TextAppearance_Yeogieotte_Pretendard_h5
+                    ),
+                    0,
+                    spannableString.length,
+                    Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                )
+            }
+            spannableStringBuilder.append(spannableString)
+
+            if (index < texts.size - 1) {
+                spannableStringBuilder.append("\n")
+            }
+        }
+        return spannableStringBuilder
     }
 }
---
+++ app/src/main/java/com/sopt/yeogieottae/ui/room/RoomViewModel.kt
@@ -0,0 +1,56 @@
+package com.sopt.yeogieottae.ui.room
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.sopt.yeogieottae.data.model.Room
+
+class RoomViewModel : ViewModel() {
+    private val _Room = MutableLiveData<Room>()
+    val Room: LiveData<Room>
+        get() = _Room
+
+    private val _detail = MutableLiveData<Detail>()
+    val detail: LiveData<Detail>
+        get() = _detail
+
+    init {
+        _Room.value = Room(
+            room_id = 1,
+            room_name = "스탠다드 트윈룸",
+            price = 156900,
+            start_time = "15:00",
+            end_time = "11:00",
+            image_url = "",
+            is_liked = true
+        )
+        _detail.value = Detail(
+            information = listOf(
+                "2인 기준 최대 3인 (유료)",
+                "인원 추가 시 비용이 발생돼요.",
+                "싱글베드 2개",
+                "객실+욕실 / 12평",
+            ),
+            facilities = listOf(
+                "TV, 쇼파, 티테이블, 사무용책상, 옷장, 에어컨, 냉장고,미니바(유료), 드라이기, 욕실용품, 샤워가운, 슬리퍼",
+            ),
+            cancel = listOf(
+                "체크인일 기준 3일전 18시까지 : 100% 환불",
+                "체크인일 기준 3일전 18시 이후~당일 및 No-show : 환불  불가 (연박의 경우 최초 1박 환불 불가, 이후 일자 100% 환불)",
+                "취소, 환불 시 수수료가 발생할 수 있습니다.",
+                "아래 객실은 별도의 취소 규정이 적용되오니 참고 부탁드립니다",
+                "[룸온리 특가], [호텔딜], [오픈런] 객실: 체크인일 기준 7일 전 18시까지 100% 환불, 이후 환불 불가 (연박의 경우 최초 1회 환불 불 이 일자 100% 환불)",
+                "[특가],[취소불가] 객실: 취소, 변경, 환불 불가",
+                "예약 후 10분 내 취소될 경우 취소 수수료가 발생하지 않습니다. (*체크인 시간 경과 후 제외)",
+                "예약 후 10분 경과 시엔 해당 숙소의 취소 및 환불 규정이 적용됩니다.",
+            )
+        )
+    }
+
+    data class Detail(
+        var information: List<String>,
+        var facilities: List<String>,
+        var cancel: List<String>,
+    )
+
+}
