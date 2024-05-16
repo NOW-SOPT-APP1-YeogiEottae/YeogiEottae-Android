c9f340925e4e061785e01ccd91aa6c83b87880e7
2hyunjinn <95455569+2hyunjinn@users.noreply.github.com>
2024-05-16 22:03:17 +0900

    [Refactor] textstyle 적용

--- app/src/main/java/com/sopt/yeogieottae/ui/search/SearchFragment.kt
+++ app/src/main/java/com/sopt/yeogieottae/ui/search/SearchFragment.kt
@@ -28,12 +28,9 @@ class SearchFragment : BaseFragment<FragmentSearchBinding>(
     // 호텔 리사이클러 뷰
     private fun initHotelListView() {
         // 호텔 리사이클러뷰 설정
-        val recyclerView: RecyclerView = requireView().findViewById(R.id.rv_searchlist_hotellist)
         val adapter = HotelListAdapter()
-        recyclerView.adapter = adapter
-        recyclerView.layoutManager = LinearLayoutManager(requireContext())
-
-        // HotelViewModel에서 호텔 목록을 관찰하고 변경되면 업데이트
+        binding.rvSearchlistHotellist.adapter = adapter
+        binding.rvSearchlistHotellist.layoutManager = LinearLayoutManager(requireContext())
         hotelViewModel.hotels.observe(viewLifecycleOwner) { hotels ->
             hotels?.let {
                 adapter.setHotels(it)
@@ -43,26 +40,16 @@ class SearchFragment : BaseFragment<FragmentSearchBinding>(
 
     // 매진 숙소 필터
     private fun initFilterExceptView() {
-        // RecyclerView 스크롤 이벤트 처리
-        val recyclerView: RecyclerView = requireView().findViewById(R.id.rv_searchlist_hotellist)
-        recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+        binding.rvSearchlistHotellist.addOnScrollListener(object : RecyclerView.OnScrollListener() {
             private var isScrolledUp = false
-
             override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
                 super.onScrolled(recyclerView, dx, dy)
-                // 스크롤 방향에 따라 아래 이미지뷰 숨기기 또는 표시하기
                 if (dy > 0 && !isScrolledUp) {
-                    // 아래로 스크롤할 때
-                    // 아래 이미지뷰 숨기기
                     isScrolledUp = true
-                    view?.findViewById<View>(R.id.iv_searchlist_filter_except)?.animate()?.alpha(0f)
-                        ?.setDuration(300)?.start()
+                    binding.ivSearchlistFilterExcept.animate()?.alpha(0f)?.setDuration(300)?.start()
                 } else if (dy < 0 && isScrolledUp) {
-                    // 위로 스크롤할 때
-                    // 아래 이미지뷰 표시하기
                     isScrolledUp = false
-                    view?.findViewById<View>(R.id.iv_searchlist_filter_except)?.animate()?.alpha(1f)
-                        ?.setDuration(300)?.start()
+                    binding.ivSearchlistFilterExcept.animate()?.alpha(1f)?.setDuration(300)?.start()
                 }
             }
         })
