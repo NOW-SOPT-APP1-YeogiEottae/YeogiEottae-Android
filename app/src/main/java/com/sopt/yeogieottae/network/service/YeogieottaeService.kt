0de48e1a4cc8c038daa06314a88973e66b086433
김명석 <twinaudtjr@naver.com>
2024-05-22 02:17:51 +0900

    [Add] #20 찜하기 dto 및 Service 추가

--- app/src/main/java/com/sopt/yeogieottae/network/service/YeogieottaeService.kt
+++ app/src/main/java/com/sopt/yeogieottae/network/service/YeogieottaeService.kt
@@ -1,12 +1,16 @@
 package com.sopt.yeogieottae.network.service
 
 import com.sopt.yeogieottae.network.response.ResponseHotelDto
+import com.sopt.yeogieottae.network.response.ResponseLikeDto
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface YeogieottaeService {
 
     @GET("/api/v1/hotels/{hotelId}")
     suspend fun getHotel(@Path("hotelId") hotelId: Int): Response<ResponseHotelDto>
+
+    suspend fun postLike(@Query("roomType") roomType: Int): Response<ResponseLikeDto>
 }
