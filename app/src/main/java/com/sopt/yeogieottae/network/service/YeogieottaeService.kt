98e8b1688952eff666053674b4b977ad5c8f7b4a
0se0 <030ggong@gmail.com>
2024-05-21 20:45:11 +0900

    [Feat]#22 - YeogieottaeService

---
+++ app/src/main/java/com/sopt/yeogieottae/network/service/YeogieottaeService.kt
@@ -0,0 +1,20 @@
+package com.sopt.yeogieottae.network.service
+
+import com.sopt.yeogieottae.network.request.RequestRoomId
+import com.sopt.yeogieottae.network.response.ResponseCompareDto
+import com.sopt.yeogieottae.network.response.ResponseCompareLikesDto
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+
+interface YeogieottaeService {
+    @GET("/api/v1/compare-rooms")
+    suspend fun compare(): Response<ResponseCompareDto>
+
+    @GET("/api/v1/compare-rooms/likes")
+    suspend fun getCompare(): Response<ResponseCompareLikesDto>
+
+    @POST("/api/v1/compare-rooms/likes")
+    suspend fun postCompare(@Body roomIdRequest: RequestRoomId): Response<Unit>
+}
