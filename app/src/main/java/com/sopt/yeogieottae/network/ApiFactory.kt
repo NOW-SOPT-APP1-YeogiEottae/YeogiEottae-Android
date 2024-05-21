bb61eac086a469963273bc27e9f4bb01964b5bd8
0se0 <030ggong@gmail.com>
2024-05-21 20:43:56 +0900

    [Feat]#22 - ApiFactory

---
+++ app/src/main/java/com/sopt/yeogieottae/network/ApiFactory.kt
@@ -0,0 +1,37 @@
+package com.sopt.yeogieottae.network
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import com.sopt.yeogieottae.BuildConfig
+import com.sopt.yeogieottae.network.service.YeogieottaeService
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+
+object ApiFactory {
+    private const val BASE_URL: String = BuildConfig.AUTH_BASE_URL
+
+    private val loggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(HeaderInterceptor())
+        .addInterceptor(loggingInterceptor)
+        .build()
+
+    val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+            .build()
+    }
+
+    inline fun <reified T> create(): T = retrofit.create(T::class.java)
+}
+
+object ServicePool {
+    val authService = ApiFactory.create<YeogieottaeService>()
+}
