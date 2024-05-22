7b958110bc51ce7b5382e03e6c0dbf5240a36434
2hyunjinn <95455569+2hyunjinn@users.noreply.github.com>
2024-05-22 18:23:59 +0900

    [Feat] room 유무에 따라 view 변경

--- app/src/main/java/com/sopt/yeogieottae/ui/like/FavoriteHotelViewHolder.kt
+++ app/src/main/java/com/sopt/yeogieottae/ui/like/FavoriteHotelViewHolder.kt
@@ -1,20 +1,64 @@
 package com.sopt.yeogieottae.ui.like
 
 import android.graphics.Paint
+import android.util.Log
+import android.view.View
 import androidx.recyclerview.widget.RecyclerView
-import com.sopt.yeogieottae.data.Like.FavoriteHotel
+import com.bumptech.glide.Glide
+import com.sopt.yeogieottae.R
+import com.sopt.yeogieottae.data.like.LikeHotelState
 import com.sopt.yeogieottae.databinding.ItemFavoriteHotelBinding
 
-class FavoriteHotelViewHolder(private val binding: ItemFavoriteHotelBinding) :
+class FavoriteHotelViewHolder(
+    private val binding: ItemFavoriteHotelBinding,
+) :
     RecyclerView.ViewHolder(binding.root) {
-    fun bind(hotelData: FavoriteHotel) {
-        binding.run {
-            tvFavoritehotelName.text = hotelData.hotelName
-            tvFavoritehoteStarText.text = hotelData.reviewRate.toString()
-            tvFavoritehotelRoomname.text = hotelData.roomName
-            ivFavoritehotelRoom.setImageResource(hotelData.roomImage)
-            tvHotelTotalPrice.text = hotelData.price.toString()
+    fun bind(likeHotelState: LikeHotelState) {
+        if (likeHotelState.isSuccess) {
+            initHotelViews(likeHotelState)
+            if (likeHotelState.roomInformation == null) {
+                showEmptyRoom()
+            } else {
+                showLikeRoom()
+                initRoomView(likeHotelState)
+            }
+        } else {
+            Log.d("likehotel", "실패..")
+        }
+    }
+
+    private fun initHotelViews(likeHotelState: LikeHotelState) {
+        with(binding) {
+            tvFavoritehotelName.text = likeHotelState.hotelName
+            tvFavoritehoteStarText.text = likeHotelState.reviewRate.toString()
+            tvFavoritehotelNameEmpty.text = likeHotelState.hotelName
+            tvFavoritehoteStarTextEmpty.text = likeHotelState.reviewRate.toString()
+        }
+    }
+
+    private fun initRoomView(likeHotelState: LikeHotelState) {
+        with(binding) {
+            tvFavoritehotelRoomname.text = likeHotelState.roomName
+            Glide.with(ivFavoritehotelRoom.context)
+                .load(likeHotelState.roomImage)
+                .into(ivFavoritehotelRoom)
+            binding.tvHotelTotalPrice.text =
+                itemView.context.getString(R.string.all_price).format(likeHotelState.price)
             tvHotelDiscountPrice.paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
+        }
+    }
+
+    private fun showLikeRoom() {
+        with(binding) {
+            loFavoritehotelLikeNotempty.visibility = View.VISIBLE
+            loFavoritehotelEmptyLikeroom.visibility = View.GONE
+        }
+    }
+
+    private fun showEmptyRoom() {
+        with(binding) {
+            loFavoritehotelLikeNotempty.visibility = View.GONE
+            loFavoritehotelEmptyLikeroom.visibility = View.VISIBLE
         }
     }
 }
