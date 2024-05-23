c5663bd76e5e4e7a7b3848d10b69f306c7b3ff34
0se0 <030ggong@gmail.com>
2024-05-24 04:13:34 +0900

    [Feat]#7 - DeleteDialogFragment

---
+++ app/src/main/java/com/sopt/yeogieottae/ui/compare/DeleteDialog.kt
@@ -0,0 +1,62 @@
+package com.sopt.yeogieottae.ui.compare
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
+import androidx.fragment.app.DialogFragment
+import com.sopt.yeogieottae.R
+import com.sopt.yeogieottae.databinding.DialogDeleteBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+class DeleteDialogFragment(
+    private val deleteCompareRoom: suspend (Int) -> Unit,
+    private val roomId: Int,
+    private val coroutineScope: CoroutineScope,
+) : DialogFragment() {
+
+    private var _binding: DialogDeleteBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        _binding = DialogDeleteBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setDialogSize()
+        dialog?.window?.setBackgroundDrawableResource(R.drawable.bg_white_radius_18dp)
+
+        binding.btnNo.setOnClickListener {
+            dismiss()
+        }
+        binding.btnYes.setOnClickListener {
+            coroutineScope.launch {
+                deleteCompareRoom(roomId)
+                dismiss()
+            }
+        }
+    }
+
+    private fun setDialogSize() {
+        val displayMetrics = resources.displayMetrics
+        val widthPixels = displayMetrics.widthPixels
+
+        val params = dialog?.window?.attributes
+        params?.width = (widthPixels * 0.9).toInt()
+        dialog?.window?.attributes = params as WindowManager.LayoutParams
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
