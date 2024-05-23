6cc7b23af6b1584e6b01e37f234e435d5a3bb359
0se0 <030ggong@gmail.com>
2024-05-23 16:12:34 +0900

    [Feat]#7 - CompareNotEmptyFragment

--- app/src/main/java/com/sopt/yeogieottae/ui/compare/CompareNotEmptyFragment.kt
+++ app/src/main/java/com/sopt/yeogieottae/ui/compare/CompareNotEmptyFragment.kt
@@ -4,36 +4,63 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import com.sopt.yeogieottae.databinding.FragmentCompareNotEmptyBinding
-import com.sopt.yeogieottae.network.response.ResponseCompareRoom
 import com.sopt.yeogieottae.ui.compare.empty.BottomSheetFragment
 import com.sopt.yeogieottae.util.BaseFragment
+import kotlinx.coroutines.launch
 
 class CompareNotEmptyFragment : BaseFragment<FragmentCompareNotEmptyBinding>(
     FragmentCompareNotEmptyBinding::inflate
 ) {
     private lateinit var compareViewModel: CompareViewModel
+    private lateinit var outerAdapter: OuterAdapter
+
+    private var isEditMode = false
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.tvAddBtn.setOnClickListener {
+        initCompareViewModel()
+        setupRecyclerView()
+        observeViewModel()
+
+        binding.ivPlusBtn.setOnClickListener {
             BottomSheetFragment().show(parentFragmentManager, "BottomSheetFragment")
         }
 
-        initCompareViewModel()
-        observeViewModel()
-        compareViewModel.fetchCompareData()
+        binding.tvBtnEdit.setOnClickListener {
+            toggleEditMode()
+        }
+
+        binding.tvBtnEditDone.setOnClickListener {
+            toggleEditMode()
+        }
     }
 
     private fun initCompareViewModel() {
         compareViewModel = ViewModelProvider(requireActivity())[CompareViewModel::class.java]
     }
 
+    private fun setupRecyclerView() {
+        outerAdapter = OuterAdapter(
+            onRoomSelected = { room ->
+                ///todo 선택된 방을 처리 하는 로직 추가
+            },
+            deleteCompareRoom = { roomId ->
+                lifecycleScope.launch {
+                    compareViewModel.deleteRoom(roomId)
+                }
+            },
+            coroutineScope = lifecycleScope
+        )
+        binding.rvCompare.adapter = outerAdapter
+    }
+
     private fun observeViewModel() {
         compareViewModel.compareResponse.observe(viewLifecycleOwner) { response ->
             response?.result?.let { roomList ->
-                setupRecyclerView(roomList)
+                outerAdapter.submitList(roomList)
             }
         }
 
@@ -42,19 +69,10 @@ class CompareNotEmptyFragment : BaseFragment<FragmentCompareNotEmptyBinding>(
         }
     }
 
-    private fun setupRecyclerView(data: List<ResponseCompareRoom>) {
-        val items = data.map {
-            OuterItem(
-                imageUrl = it.imageUrl,
-                roomName = it.roomName,
-                hotelName = it.hotelName,
-                details = InnerItem(
-                    money = "${it.price}원",
-                    starRate = "${it.reviewRate}",
-                    reviewCount = "(${it.reviewCount})"
-                )
-            )
-        }
-        binding.rvCompare.adapter = OuterAdapter(items)
+    private fun toggleEditMode() {
+        isEditMode = !isEditMode
+        binding.tvBtnEdit.visibility = if (isEditMode) View.INVISIBLE else View.VISIBLE
+        binding.tvBtnEditDone.visibility = if (isEditMode) View.VISIBLE else View.INVISIBLE
+        outerAdapter.setEditMode(isEditMode)
     }
 }
