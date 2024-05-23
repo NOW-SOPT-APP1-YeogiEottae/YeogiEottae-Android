4e2eebbd728e97480784b22f454f65bbd2eb1f86
0se0 <030ggong@gmail.com>
2024-05-23 16:12:07 +0900

    [Feat]#7 - InnerViewHolder

--- app/src/main/java/com/sopt/yeogieottae/ui/compare/InnerViewHolder.kt
+++ app/src/main/java/com/sopt/yeogieottae/ui/compare/InnerViewHolder.kt
@@ -2,11 +2,22 @@ package com.sopt.yeogieottae.ui.compare
 
 import androidx.recyclerview.widget.RecyclerView
 import com.sopt.yeogieottae.databinding.ItemCompareDetailBinding
+import com.sopt.yeogieottae.network.response.ResponseCompareRoom
 
-class InnerViewHolder(private val binding: ItemCompareDetailBinding) : RecyclerView.ViewHolder(binding.root) {
-    fun bind(item: InnerItem) {
-        binding.tvMoney.text = item.money
-        binding.tvStar.text = item.starRate
-        binding.tvReviewCount.text = item.reviewCount
+class InnerViewHolder(
+    private val binding: ItemCompareDetailBinding,
+    private val onItemClicked: (ResponseCompareRoom) -> Unit
+) : RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(item: ResponseCompareRoom) {
+        binding.apply {
+            tvPrice.text = item.price.toString()
+            tvReviewRate.text = item.reviewRate.toString()
+            tvReviewCount.text = item.reviewCount.toString()
+
+            root.setOnClickListener {
+                onItemClicked(item)
+            }
+        }
     }
 }
