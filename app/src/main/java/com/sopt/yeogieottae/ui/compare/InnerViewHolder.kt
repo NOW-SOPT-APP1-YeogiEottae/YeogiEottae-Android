104eea6ab8578db3d16f145b541c428fbaebf2a1
0se0 <030ggong@gmail.com>
2024-05-24 04:14:43 +0900

    [Feat]#7 - 텍스트 조정

--- app/src/main/java/com/sopt/yeogieottae/ui/compare/InnerViewHolder.kt
+++ app/src/main/java/com/sopt/yeogieottae/ui/compare/InnerViewHolder.kt
@@ -1,19 +1,29 @@
 package com.sopt.yeogieottae.ui.compare
 
+import android.graphics.Paint
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.sopt.yeogieottae.R
 import com.sopt.yeogieottae.databinding.ItemCompareDetailBinding
 import com.sopt.yeogieottae.network.response.ResponseCompareRoom
+import java.text.DecimalFormat
+import java.text.NumberFormat
+import java.util.Locale
 
 class InnerViewHolder(
     private val binding: ItemCompareDetailBinding,
-    private val onItemClicked: (ResponseCompareRoom) -> Unit
+    private val onItemClicked: (ResponseCompareRoom) -> Unit,
 ) : RecyclerView.ViewHolder(binding.root) {
 
     fun bind(item: ResponseCompareRoom) {
-        binding.apply {
-            tvPrice.text = item.price.toString()
+        with(binding) {
+            val priceFormat = NumberFormat.getNumberInstance(Locale.getDefault())
+            val reviewCountWithoutDecimal = DecimalFormat("#").format(item.reviewCount)
+
+            tvMoneyBasic.paintFlags = (tvMoneyBasic.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG)
+            tvPrice.text = priceFormat.format(item.price) + "원"
             tvReviewRate.text = item.reviewRate.toString()
-            tvReviewCount.text = item.reviewCount.toString()
+            tvReviewCount.text = "($reviewCountWithoutDecimal)"
 
             root.setOnClickListener {
                 onItemClicked(item)
