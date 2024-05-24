08f5421ab070ae8ead41a44562b86e926ddc6b78
0se0 <030ggong@gmail.com>
2024-05-24 19:53:59 +0900

    [Rename]#35 - 카멜 케이스로 수정

--- app/src/main/java/com/sopt/yeogieottae/network/mapper/HotelMapper.kt
+++ app/src/main/java/com/sopt/yeogieottae/network/mapper/HotelMapper.kt
@@ -7,26 +7,26 @@ import com.sopt.yeogieottae.ui.hotel.HotelViewModel.Hotel
 object HotelMapper {
     fun fromDto(dto: ResponseHotelDto.Hotel): Hotel {
         return Hotel(
-            name = dto.hotel_name,
+            name = dto.hotelName,
             star = dto.star,
             location = dto.location,
-            review_rate = dto.review_rate,
-            review_count = dto.review_count,
-            is_liked = dto.is_liked,
-            room_list = fromDto(dto.room_list)
+            reviewRate = dto.reviewRate,
+            reviewCount = dto.reviewCount,
+            isLiked = dto.isLiked,
+            roomList = fromDto(dto.roomList)
         )
     }
 
-    fun fromDto(dto: List<ResponseHotelDto.Hotel.Room>): List<Room> {
+    private fun fromDto(dto: List<ResponseHotelDto.Hotel.Room>): List<Room> {
         return dto.map { room ->
             Room(
-                room_id = room.room_id,
-                room_name = room.room_name,
+                roomId = room.roomId,
+                roomName = room.roomName,
                 price = room.price,
-                start_time = room.start_time,
-                end_time = room.end_time,
-                image_url = room.image_url,
-                is_liked = room.is_liked
+                startTime = room.startTime,
+                endTime = room.endTime,
+                imageUrl = room.imageUrl,
+                isLiked = room.isLiked
             )
         }
     }
